f9d378f8de5640f4a7dba1043f9478f495385aa5
adityain <adityain98@gmail.com>
2024-12-13 23:59:03 +0700

    test https

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+const https = require('https')
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = 3000
+
+const options = {
+  key: fs.readFileSync('private-key.pem'),
+  cert: fs.readFileSync('certificate.pem'),
+};
 
 app.get('/', (req, res) => {
   res.send('Hello World! from appjs 123123')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// https.createServer(options, app).listen(443);
+
+var httpsServer = https.createServer(options, app)
+
+httpsServer.listen(8443)
